4428acc9eb418c14a2cc0a3fafb38f4bf910a4a6
Omar Espinoza <neuralmancer@hotmail.com>
2015-03-18 15:12:33 -0600

    cambios de la clase del 18 de marzo
    cambios para terminar la clase de manejo de templetes y formas

--- hello.py
+++ hello.py
@@ -1,21 +1,50 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
+#from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'muppet'
+#bootstrap = Bootstrap(app)
+
+class NameForm(Form):
+    nombre = StringField('Escribe tu nombre:', validators=[Required()])
+    submit = SubmitField('Enviar!')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return "Hola Mundo!"
+    #return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+#ejemplo de forma
+@app.route('/forma', methods=['GET', 'POST'])
+def forma():
+    nombre = None
+    form = NameForm()
+    if form.validate_on_submit():
+        nombre = form.nombre.data
+        form.nombre.data = ''
+    return render_template("forma.html", form = form, nombre = nombre)
 
+#template que se llena desde un archivo de texto
+@app.route('/archivo')
+def archivo():
+    texto = []
+    f = open('texto.txt', 'r')
+    for linea in f:
+        linea = linea.strip()
+        texto.append(linea)
+    f.close()
+    return render_template("archivo.html", texto = texto)
+
+#ejemplo pasar valor por url, en template
 @app.route('/usuario/<usuario>')
 def user_name(usuario):
-    return "Hola {0}!".format(usuario)
+    return render_template("usuario.html", usuario = usuario)
 
+#404 personalizado
 @app.errorhandler(404)
 def pagina_no_encontrada(e):
     return render_template("404.html")
